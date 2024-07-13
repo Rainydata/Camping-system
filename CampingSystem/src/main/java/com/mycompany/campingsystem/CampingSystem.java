6edc948f2e3705f22e7ed131f92b199cd06bb2e6
Rainydata <benavidesbryan403@gmail.com>
2024-07-13 14:21:12 -0500

    Firt commit into the main branch

---
+++ CampingSystem/src/main/java/com/mycompany/campingsystem/CampingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.campingsystem;
+
+/**
+ *
+ * @author benav
+ */
+public class CampingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
